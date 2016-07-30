ba9332b107b89daf64eac695ae1c512f65626600
bhagathreddy123 <katta_reddy04@yahoo.com>
2016-07-30 18:14:19 +0530

    added kola gem but not fetching freiends list

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,4 +47,15 @@ class User < ActiveRecord::Base
 # end
 
 
+    def facebook
+      @facebook ||= Koala::Facebook::API.new(oauth_token)
+      block_given? ? yield(@facebook) : @facebook
+    rescue Koala::Facebook::APIError => e
+      logger.info.e.to_s
+      nil
+    end
+
+    def friends_count
+      facebook { |fb| fb.get_connection("me","friends").size }
+end
 end
